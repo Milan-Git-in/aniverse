a04fb0257df9fd35642df6016858d245f339b5da
Milan-Prajapati <milanprajapati366@gmail.com>
2025-05-08 15:45:18 +0530

    Initial

---
+++ Mobile/aniverse/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-3xl font-bold underline">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
