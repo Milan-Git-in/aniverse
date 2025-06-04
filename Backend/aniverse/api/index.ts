bdb40b9711eb28a8fa580e7b8d2d83b4290fd402
Milan-Prajapati <milanprajapati366@gmail.com>
2025-06-04 15:53:46 +0530

    add vercel

---
+++ Backend/aniverse/api/index.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const expressApp = express();
+
+const createNestServer = async (expressInstance: express.Express) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  await app.init();
+};
+
+createNestServer(expressApp);
+
+export default expressApp;
