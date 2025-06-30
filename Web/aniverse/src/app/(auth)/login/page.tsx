b9617b448e4863a6170fa8715fef2fc4a207ca84
Milan-Prajapati <milanprajapati366@gmail.com>
2025-06-30 16:51:08 +0530

    create structure

--- Web/aniverse/src/app/(auth)/login/page.tsx
+++ Web/aniverse/src/app/(auth)/login/page.tsx
@@ -1,7 +1,8 @@
+"use client";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const LoginPage = () => {
+  return <div>LoginPage</div>;
 };
 
-export default page;
+export default LoginPage;
