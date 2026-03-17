48da51627693ab5cdf702750f3b13b4ed688f863
Milan Prajapati <milanprajapati366@gmail.com>
2026-03-17 16:27:01 +0530

    change the url

--- Web/aniverse/next.config.ts
+++ Web/aniverse/next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
-    NEXT_PUBLIC_URL: "http://localhost:3000",
-    NEXT_PUBLIC_HOST_URL: "https://web-aniverse.vercel.app",
+    NEXT_PUBLIC_URL: "https://web-aniverse.vercel.app",
+    NEXT_PUBLIC_TEST_URL: "https://localhost:3000",
   },
   images: {
     remotePatterns: [
